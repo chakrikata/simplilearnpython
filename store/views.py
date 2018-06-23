4c058e746aa4eb136c68bc6726264b5f1f346a9f
chakrkata <chakrikata@gmail.com>
2018-06-23 20:37:16 +0530

    mgrated folder added

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def index(request):
+    return render(request,'template.html')
+
+
+def store(request):
+    count = Book.objects.all().count()
+    context = {
+        'count': count,
+    }
+    return render(request,'store.html', context)
