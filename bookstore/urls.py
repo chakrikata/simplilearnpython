20080965be23fad68d7adcb4ee90f07010b5ac27
chakrkata <chakrikata@gmail.com>
2018-06-26 13:40:31 +0530

    added migration

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     url(r'^store/', views.store, name='store'),
     url(r'^$', views.store, name='index'),
     url(r'^book/(\d+)', views.book_details, name='book_details'),
+    url(r'^add/(\d+)', views.add_to_cart, name='add_to_cart'),
+    url(r'^remove/(\d+)', views.remove_from_cart, name='remove_from_cart'),
+    url(r'^cart/', views.cart, name='cart'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^admin/', admin.site.urls),
 ]
